6d98c158c901ebee6bef1f488057484f220fc8e8
littlesophie2000 <79338024+littlesophie2000@users.noreply.github.com>
2021-02-22 22:12:20 -0500

    Uploading files for final

---
+++ sketch.js
@@ -0,0 +1,36 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  textSize(15);
+  background(0);
+  rSlider = createSlider(0, 255, 100);
+  rSlider.position(20, 20);
+  gSlider = createSlider(0, 255, 0);
+  gSlider.position(20, 50);
+  bSlider = createSlider(0, 255, 255);
+  bSlider.position(20, 80);
+}
+
+function draw() {
+  background(0);
+  const r = rSlider.value();
+  const g = gSlider.value();
+  const b = bSlider.value();
+  background(r, g, b);
+  text('Red', rSlider.x * 2 + rSlider.width, 35);
+  text('Green', gSlider.x * 2 + gSlider.width, 65);
+  text('Blue', bSlider.x * 2 + bSlider.width, 95);
+
+
+C = 0
+
+   noStroke()
+    translate(windowWidth / 2, windowWidth / 2)
+    S = 2 + abs(cos(C++/20))
+      scale(S);
+  rotate(PI / 4) 
+  fill(S * 80, 0, 0)
+  rect(-25, -25, 100, 100)
+  rect(-25, -25, 100, 100) 
+  circle(25, -25, 100) 
+  circle(-25, 25, 100)
+    }
